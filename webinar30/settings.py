842ec2a432f46090dbec0a1a035b4a18229ee91e
AlymkanKalykova <alymkan23@gmail.comgit config --global user.name >
2024-07-18 14:38:22 +0600

    changes the database to postgres

--- webinar30/settings.py
+++ webinar30/settings.py
@@ -75,12 +75,19 @@ WSGI_APPLICATION = 'webinar30.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'todolist',
+        'USER': 'alymkankalykova',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
