0db0d1d759787ffb989fdb16e82482ab1858a3e4
AlymkanKalykova <alymkan23@gmail.comgit config --global user.name >
2024-07-18 15:31:23 +0600

    updated html pages

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from webapp.views import index, create_task, task_detail
 
 urlpatterns = [
-    path('', index, name='tasks'),
+    path('', index, name='index'),
     path('create/', create_task, name='create_task'),
     path('task/<int:pk>/', task_detail, name='task_detail'),
 ]
