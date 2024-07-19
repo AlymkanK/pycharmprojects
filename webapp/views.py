e34549477d9612af7cf594490db0f593feb550dd
AlymkanKalykova <alymkan23@gmail.comgit config --global user.name >
2024-07-19 15:09:03 +0600

    created update and delete options on main page

--- webapp/views.py
+++ webapp/views.py
@@ -1,21 +1,24 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 
 from webapp.models import Task, status_choices
+
 
 
 # Create your views here.
 
 
 def index(request):
-    tasks = Task.objects.order_by()
+    tasks = Task.objects.order_by('date')
     return render(request, "index.html", context={"tasks": tasks})
 
 
 def create_task(request):
     if request.method == "GET":
-        return render(request, "create_task.html", {'status_choices': status_choices})
+        return render(request, "create_task.html",
+                      context= {'task':get_object_or_404(Task, pk=1)}
+                      )
     else:
         Task.objects.create(
             description=request.POST.get("description"),
@@ -28,3 +31,25 @@ def create_task(request):
 def task_detail(request, *args, pk, **kwargs):
     task = get_object_or_404(Task, pk=pk)
     return render(request, "task_detail.html", context={"task": task})
+
+
+def update_task(request, *args, pk, **kwargs):
+    if request.method == "GET":
+        return render(request, "update_task.html", {'status_choices': status_choices})
+    else:
+        Task.objects.create(
+            description=request.POST.get("description"),
+            status=request.POST.get("status"),
+            date=request.POST.get("date")
+        )
+        return redirect('task_detail', pk=pk)
+
+
+def delete_task(request, *args, pk, **kwargs):
+    if request.method == "GET":
+        task = get_object_or_404(Task, pk=pk)
+        return render(request, "delete_task.html", context={"task": task})
+    else:
+        task = get_object_or_404(Task, pk=pk)
+        task.delete()
+        return redirect('tasks')
